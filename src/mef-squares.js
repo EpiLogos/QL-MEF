75be99499b4da0065f4ec1003c3097d7c2ca2082
Epii <frank.g.taylor97@gmail.com>
2026-08-14 00:40:21 +0100

    feat(q2): add MEF square index

---
+++ src/mef-squares.js
@@ -0,0 +1,5 @@
+export const MEF_SQUARES = Object.freeze({
+  A: Object.freeze({ name: "Articulation", lenses: Object.freeze(["L0", "L0′", "L5", "L5′"]) }),
+  B: Object.freeze({ name: "Encounter", lenses: Object.freeze(["L1", "L1′", "L4", "L4′"]) }),
+  C: Object.freeze({ name: "Becoming", lenses: Object.freeze(["L2", "L2′", "L3", "L3′"]) })
+});
