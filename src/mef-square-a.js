e45813e6241feec2cc04b4983b1b72f16470efe1
Epii <frank.g.taylor97@gmail.com>
2026-08-14 00:38:20 +0100

    feat(q2): add MEF Square A

---
+++ src/mef-square-a.js
@@ -0,0 +1,7 @@
+import { mefLens } from "./mef-entry.js";
+export const MEF_SQUARE_A = Object.freeze([
+  mefLens("L0", "Quaternal", 0, "day", "A", "L5", "L5′", ["Why", "What", "How", "Whom/Which/When", "Where/Why-fo(u)r", "Why-so/Why-not"]),
+  mefLens("L0′", "Archetypal-Numerical", 0, "night", "A", "L5′", "L5", ["One/Unity", "Two/Duality", "Three/Triad", "Four/Quaternity", "Five/Pentad", "Six/Hexad"]),
+  mefLens("L5", "Para Vāk", 5, "day", "A", "L0", "L0′", ["Anuttara/Asambhava", "Para Vāk", "Paśyantī", "Madhyamā", "Vaikharī", "Mātṛkā"]),
+  mefLens("L5′", "Divine Logos", 5, "night", "A", "L0′", "L0", ["Arche", "Apokalypsis", "Dynamis", "Sophia", "Parousia", "Epi-Logos"])
+]);
