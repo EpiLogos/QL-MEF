422ed78d3b3ddf2865a8342660f63df6a1264776
Epii <frank.g.taylor97@gmail.com>
2026-08-14 01:40:50 +0100

    style(q2): rustfmt becoming registry

--- crates/ql-mef/src/registry/becoming.rs
+++ crates/ql-mef/src/registry/becoming.rs
@@ -2,18 +2,47 @@ use crate::LensId;
 
 use super::LensDefinition;
 
-pub const L2: LensDefinition = LensDefinition::new(LensId::L2, "Logical", [
-    "Tetralemmaic ground", "IS", "IS-NOT", "BOTH", "NEITHER", "SILENCE",
-]);
+pub const L2: LensDefinition = LensDefinition::new(
+    LensId::L2,
+    "Logical",
+    [
+        "Tetralemmaic ground",
+        "IS",
+        "IS-NOT",
+        "BOTH",
+        "NEITHER",
+        "SILENCE",
+    ],
+);
 
-pub const L2_PRIME: LensDefinition = LensDefinition::new(LensId::L2Prime, "Alchemical-Elemental", [
-    "Aether", "Earth", "Water", "Air", "Fire", "Salt",
-]);
+pub const L2_PRIME: LensDefinition = LensDefinition::new(
+    LensId::L2Prime,
+    "Alchemical-Elemental",
+    ["Aether", "Earth", "Water", "Air", "Fire", "Salt"],
+);
 
-pub const L3: LensDefinition = LensDefinition::new(LensId::L3, "Processual", [
-    "Concrescent desire", "Actual occasion", "Ingression", "Eternal objects", "Community integration", "Satisfaction/Perishing",
-]);
+pub const L3: LensDefinition = LensDefinition::new(
+    LensId::L3,
+    "Processual",
+    [
+        "Concrescent desire",
+        "Actual occasion",
+        "Ingression",
+        "Eternal objects",
+        "Community integration",
+        "Satisfaction/Perishing",
+    ],
+);
 
-pub const L3_PRIME: LensDefinition = LensDefinition::new(LensId::L3Prime, "Chronological", [
-    "Spirit (Geist)", "Spring", "Summer", "Autumn", "Winter", "Life (Aufhebung)",
-]);
+pub const L3_PRIME: LensDefinition = LensDefinition::new(
+    LensId::L3Prime,
+    "Chronological",
+    [
+        "Spirit (Geist)",
+        "Spring",
+        "Summer",
+        "Autumn",
+        "Winter",
+        "Life (Aufhebung)",
+    ],
+);
